5db767e1f931346f797e86fffd2151021e5ad4f5
Adrian Grimm <usmcamgrimm@gmail.com>
2021-11-04 17:36:07 -0400

    Add success page for form submission

---
+++ src/pages/FormSuccess.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+import styled from '@emotion/styled'
+
+const SuccessContainer = styled.div`
+position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+const SuccessBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const FormSent = styled.p`
+  font-family: grenze;
+  color: #ffcd00;
+  font-size: clamp(1.5rem, 5vw, 2.2rem);
+  text-align: center;
+`;
+
+export default function FormSuccess() {
+  return(
+    <Layout>
+      <SuccessContainer>
+        <SuccessBox>
+          <FormSent>
+            Thank you for contacting Havamal!
+          </FormSent>
+
+          <FormSent>
+            Your message has been sent.
+          </FormSent>
+        </SuccessBox>
+      </SuccessContainer>
+    </Layout>
+  )
+}
