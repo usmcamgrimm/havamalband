9dddc110fe07bfe8971ce5d156422b6ed469f657
Adrian Grimm <usmcamgrimm@gmail.com>
2021-11-08 11:58:04 -0500

    Import CSS for the form, fix the 'required' labels

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
 import { navigate } from 'gatsby'
+import './form.css'
 
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
