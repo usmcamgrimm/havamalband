15a3ad296cd9d0d5ec829612eeb797dda02057b8
Adrian Grimm <usmcamgrimm@gmail.com>
2021-12-08 14:01:21 -0500

    Update the text color of the sucess message

--- src/pages/FormSuccess.js
+++ src/pages/FormSuccess.js
@@ -17,7 +17,7 @@ const SuccessBox = styled.div`
 `;
 const FormSent = styled.p`
   font-family: grenze;
-  color: #ffcd00;
+  color: #66add9;
   font-size: clamp(1.5rem, 5vw, 2.2rem);
   text-align: center;
 `;
