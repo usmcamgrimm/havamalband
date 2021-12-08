6bdee5af42fea338add115ba3f75896c20a268bf
Adrian Grimm <usmcamgrimm@gmail.com>
2021-12-08 11:42:03 -0500

    Removed the date from post titles

--- src/pages/news.js
+++ src/pages/news.js
@@ -49,7 +49,6 @@ const BlogIndex = ({ data, location }) => {
                       <span itemProp="headline">{title}</span>
                     </Link>
                   </h2>
-                  <small>{post.frontmatter.date}</small>
                 </header>
                 <section>
                   <PostDesc
