5d76f779037255e6151479ef3a9a52c37ba1305d
Adrian Grimm <usmcamgrimm@gmail.com>
2022-05-27 12:00:04 -0400

    Add the band photo for the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,12 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <Image 
+          src="/images/bandPhoto.jpg"
+          height={533}
+          width={780}
+          alt="The members of Havamal"
+        />
         <h1 className={styles.title}>
           Hail, Hordes of Havamal!
         </h1>
