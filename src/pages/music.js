90dd6a1d62810d7b7f475d9a375c94568be8298d
Adrian Grimm <usmcamgrimm@gmail.com>
2022-05-30 13:18:44 -0400

    Add a CSS reset
    Trying out the one written and shared by Josh W Comeau

--- src/pages/music.js
+++ src/pages/music.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
+import Layout from '../components/layout/Layout'
 
 export default function Music() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>
           HAVAMAL | Music
         </title>
       </Head>
       <h1>Music</h1>
-    </>
+    </Layout>
   )
 }
