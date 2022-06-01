33e6b0964a77a1b937c195ad6aee046cfeeb2766
Adrian Grimm <usmcamgrimm@gmail.com>
2022-06-01 09:30:48 -0400

    Import the fonts so we can use them

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import '../styles/fonts.css'
+import '@fontsource/grenze'
+import '@fontsource/inter'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
