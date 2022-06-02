644e3255530fca8e103a00868290c1c6e19807a3
Adrian Grimm <usmcamgrimm@gmail.com>
2022-06-02 11:55:06 -0400

    Corrected the linked div for Raids

--- src/components/raids/Raids.js
+++ src/components/raids/Raids.js
@@ -1,6 +1,5 @@
 import JSONData from './raids.json'
 import styles from './raids.module.css'
-import styled from '@emotion/styled'
 
 export default function Raids() {
   return (
@@ -11,16 +10,17 @@ export default function Raids() {
             key={`raids_id_${index}`}
             className={styles.RaidWrapper}
           >
-            <div
-              className={styles.RaidContainer}
-              as="a"
-              href={raids.link}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className={styles.DateTitle}>{raids.date}</div>
-              <div className={styles.EventTitle}>{raids.event}</div>
-              <div className={styles.LocationTitle}>{raids.location}</div>
+            <div>
+              <a
+                href={raids.link}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.RaidContainer}
+              >
+                <div className={styles.DateTitle}>{raids.date}</div>
+                <div className={styles.EventTitle}>{raids.event}</div>
+                <div className={styles.LocationTitle}>{raids.location}</div>
+              </a>
             </div>
           </div>
         )
