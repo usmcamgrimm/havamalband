e64e7cac0721c58995639c293502e3c6cd53f0fd
Adrian Grimm <usmcamgrimm@gmail.com>
2022-06-11 13:48:30 -0400

    Finished the music page

--- src/pages/music.js
+++ src/pages/music.js
@@ -1,6 +1,28 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Layout from '../components/layout/Layout'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/music.module.css'
+import styled from '@emotion/styled'
+import { css } from '@emotion/css'
+
+const AlbumImage = styled.span`
+  & > span {
+    box-shadow: 2px 4px 12px #000000;
+    border-radius: 5px;
+  }
+  @media screen and (max-width: 500px) {
+    margin: 0 auto;
+    max-width: 90%;
+  }
+`;
+
+const Line = styled.hr`
+  width: 48%;
+  margin: 0 auto;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  border-bottom: 1px solid rgba(147, 147, 147, 0.4);
+`;
 
 export default function Music() {
   return (
@@ -10,7 +32,121 @@ export default function Music() {
           HAVAMAL | Music
         </title>
       </Head>
-      <h1 className={styles.pageTitle}>The Hymns of Havamal</h1>
+      <h1 className={styles.pageTitle}>
+        The Hymns of Havamal
+      </h1>
+
+      <section id="albumTSC">
+        <div className={styles.albumContainer}>
+          <AlbumImage>
+            <Image
+              src="/images/albums/tsc.jpg"
+              height={500}
+              width={500}
+              alt="The Shadow Chapter album cover"
+            />
+          </AlbumImage>
+          <div className="albumDetails">
+            <h2 className={styles.albumTitle}>
+              The Shadow Chapter
+            </h2>
+            <h3 className={styles.releaseInfo}>
+              Art Gates Records | May 2021
+            </h3>
+            <iframe
+              title="The Shadow Chapter Sample on Spotify"
+              src="https://open.spotify.com/embed/album/1I4MAMVeh0dqfe0tz1f5Gh"
+              width="325"
+              height="360"
+              frameBorder="0"
+              allowtransparency="true"
+              allow="encrypted-media"
+              className={css`
+                margin-left: 2rem;
+                @media screen and (max-width: 768px){
+                  margin-left: 0;
+                }
+              `}
+            ></iframe>
+          </div>
+        </div>
+
+        <Line />
+      </section>
+
+      <section id="albumTFY">
+        <div className={styles.albumContainer}>
+         <AlbumImage>
+          <Image
+              src="/images/albums/tfy.jpg"
+              height={500}
+              width={500}
+              alt="Tales From Yggdrasil album cover"
+            />
+         </AlbumImage>
+          <div className="albumDetails">
+            <h2 className={styles.albumTitle}>
+              Tales From Yggdrasil
+            </h2>
+            <h3 className={styles.releaseInfo}>
+              Art Gates Records | June 2019
+            </h3>
+            <iframe
+              title="Tales From Yggdrasil on Spotify"
+              src="https://open.spotify.com/embed/album/6WIQW0JtBAgoKuUuuwclcP"
+              width="325"
+              height="360"
+              frameBorder="0"
+              allowtransparency="true"
+              allow="encrypted-media"
+              className={css`
+                margin-left: 2rem;
+                @media screen and (max-width: 768px){
+                  margin-left: 0;
+                }
+              `}
+            ></iframe>
+          </div>
+        </div>
+
+        <Line />
+      </section>
+
+      <section id="albumCOTN">
+        <div className={styles.albumContainer}>
+          <AlbumImage>
+            <Image
+              src="/images/albums/cotn.jpg"
+              height={500}
+              width={500}
+              alt="Call of The North album cover"
+            />
+          </AlbumImage>
+          <div className="albumDetails">
+            <h2 className={styles.albumTitle}>
+              Call of The North
+            </h2>
+            <h3 className={styles.releaseInfo}>
+              Independent | June 2017
+            </h3>
+            <iframe
+              title="Cal of The North on Spotify"
+              src="https://open.spotify.com/embed/album/2GrDVeDkYT0IEIHiwNeaQz"
+              width="325"
+              height="360"
+              frameBorder="0"
+              allowtransparency="true"
+              allow="encrypted-media"
+              className={css`
+                margin-left: 2rem;
+                @media screen and (max-width: 768px){
+                  margin-left: 0;
+                }
+              `}
+            ></iframe>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
