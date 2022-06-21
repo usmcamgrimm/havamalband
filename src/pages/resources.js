332ef496592a0fb5104ae3adeeefd36b79960c8e
Adrian Grimm <usmcamgrimm@gmail.com>
2022-06-21 18:55:09 -0400

    Import the form and links to the resources page
    The form still needs some work

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Layout from '../components/layout/Layout'
 import styles from '../styles/resources.module.css'
 import ContactForm from '../components/ContactForm/ContactForm'
+import ContactLinks from '../components/ContactLinks/ContactLinks'
 
 
 export default function Resources() {
@@ -15,7 +16,20 @@ export default function Resources() {
       <h1 className={styles.pageTitle}>
         Contact Information
       </h1>
-      <ContactForm />
+
+      <section id="contactArea">
+        <p className={styles.contactHeader}>
+          Submit the form or click a link to send a message to Havamal!
+        </p>
+        <div className={styles.contactContainer}>
+          <div className={styles.contactBox}>
+            <ContactForm />
+          </div>
+          <div className={styles.contactBox}>
+            <ContactLinks />
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
