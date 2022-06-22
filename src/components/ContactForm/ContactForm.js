60ea17e73f3d7083650724daaeda3269d2a8c11b
Adrian Grimm <usmcamgrimm@gmail.com>
2022-06-22 10:31:06 -0400

    Finished the contact form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -2,9 +2,6 @@ import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
 import styles from './form.module.css'
 
-import { css } from '@emotion/react'
-import styled from '@emotion/styled'
-
 export default function ContactForm() {
   const encode = (data) => {
     return Object.keys(data)
@@ -51,74 +48,43 @@ export default function ContactForm() {
           name="contactForm"
           data-netlify={true}
           data-netlify-honeypot="bot-field"
-          css={css`
-            width: 100%;
-          `}
         >
           <Field type="hidden" name="contactForm" />
           <Field type="hidden" name="bot-field" />
-          <label htmlFor="name">
+          <label htmlFor="name" className={styles.label}>
             <p className={styles.formLabel}>
               Your Name
             </p>
-            <ErrorMessage name="name" css={css`color: #ce1126;`} />
+            <ErrorMessage name="name" />
           </label>
           <Field
-            css={css`
-              focus {
-                outline: 1px solid #66add9;
-              }
-              border-radius: 5px;
-              padding: 8px;
-              margin: 8px 0 12px 0;
-              width: 100%;
-              font-family: grenze;
-            `}
+            className={styles.formField}
             name="name"
             type="text"
             placeholder="Name"
           />
 
-          <label htmlFor="email">
+          <label htmlFor="email" className={styles.label}>
             <p className={styles.formLabel}>
               Email Address
             </p>
             <ErrorMessage name="email" />
           </label>
           <Field
-            css={css`
-              focus {
-                outline: 1px solid #66add9;
-              }
-              border-radius: 5px;
-              padding: 8px;
-              margin: 8px 0 12px 0;
-              width: 100%;
-              font-family: grenze;
-            `}
+            className={styles.formField}
             name="email"
             type="email"
             placeholder="Email address"
           />
 
-          <label htmlFor="message">
+          <label htmlFor="message" className={styles.label}>
             <p className={styles.formLabel}>
               Message
             </p>
             <ErrorMessage name="message" />
           </label>
           <Field
-            css={css`
-              width: 100%;
-              height: 6rem;
-              margin-top: 8px;
-              padding: 8px;
-              border-radius: 5px;
-              font-family: grenze;
-              focus {
-                outline: 1px solid #66add9;
-              }
-            `}
+            className={styles.formTextField}
             as="text-area"
             component="textarea"
             name="message"
