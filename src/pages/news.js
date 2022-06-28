0666555f1df94d00c007f8a326640eb3d110bcc8
Adrian Grimm <usmcamgrimm@gmail.com>
2022-06-28 14:55:09 -0400

    Update the news page to show posts

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
-import Layout from '../components/layout/Layout'
 import styles from '../styles/Home.module.css'
+import fs from 'fs'
+import matter from 'gray-matter'
+import Link from 'next/link'
+import Image from 'next/image'
 
-export default function News() {
+export default function News({ posts }) {
   return (
     <>
       <Head>
@@ -11,6 +14,38 @@ export default function News() {
         </title>
       </Head>
       <h1 className={styles.pageTitle}>Havamal Updates</h1>
+
+      <main>
+        {posts.map(post => {
+          const { slug, frontmatter } = post
+          const { title, author, date, image } = frontmatter
+
+          return <article key={title}>
+            <Link href={`/posts/${slug}`}>
+              <h2>{title}</h2>
+            </Link>
+              <h3>By: {author}</h3>
+          </article>
+        })}
+      </main>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync('posts')
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '')
+    const readFile = fs.readFileSync(`posts/${filename}`, 'utf-8')
+    const { data: frontmatter } = matter(readFile)
+
+    return { slug, frontmatter }
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
