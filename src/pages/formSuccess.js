07be6ed5871b7e1e89795441b664b85751b05f98
Adrian Grimm <usmcamgrimm@gmail.com>
2022-07-13 12:53:14 -0400

    Finish the 'Form Success' page

--- src/pages/formSuccess.js
+++ src/pages/formSuccess.js
@@ -1,4 +1,27 @@
 import Head from 'next/head'
+import styled from '@emotion/styled'
+
+const SuccessContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%)
+`;
+
+const SuccessBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FormSent = styled.p`
+  display: flex;
+  font-family: grenze;
+  color: #66add9;
+  font-size: clamp(1.5rem, 5vw, 2.2rem);
+  text-align: center;
+`;
 
 export default function FormSuccess() {
   return (
@@ -8,7 +31,18 @@ export default function FormSuccess() {
           HAVAMAL | Success
         </title>
       </Head>
-      <h1>Your message has been sent to Havamal!</h1>
+
+      <SuccessContainer>
+        <SuccessBox>
+          <FormSent>
+            Your message has been sent.
+          </FormSent>
+
+          <FormSent>
+            Thank you for contacting Havamal!
+          </FormSent>
+        </SuccessBox>
+      </SuccessContainer>
     </>
   )
 }
