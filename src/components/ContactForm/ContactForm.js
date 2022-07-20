956c17611ce9ac6cd5b295eb101bfe81c0045b0d
Adrian Grimm <usmcamgrimm@gmail.com>
2022-07-20 15:11:53 -0400

    Adding Formspree and update contact form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,105 +1,38 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik'
-import * as Yup from 'yup'
+import { useForm, ValidationError } from '@formspree/react';
 import styles from './form.module.css'
 
 export default function ContactForm() {
-  const encode = (data) => {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
+  const [state, handleSubmit] = useForm("xdobqzkz");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
   }
   return (
-    <Formik
-      initialValues={{
-        name: '',
-        email: '',
-        message: '',
-      }}
-      validationSchema={Yup.object({
-        name: Yup.string()
-          .max(40, 'Must be 40 characters or less')
-          .required('Required'),
-        email: Yup.string()
-          .email('Must be a valid email address')
-          .required('Required'),
-        message: Yup.string()
-          .required('Required'),
-      })}
-      onSubmit={
-        (values, actions) => {
-          fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contactForm", ...values })
-          })
-          .then(() => {
-            navigate("/FormSuccess")
-            actions.resetForm()
-          })
-          .catch(() => {
-            alert('Error');
-          })
-          .finally(() => actions.setSubmitting(false))
-        }
-      }
-    >
-      <div className={styles.formWrapper}>
-        <Form
-          name="contactForm"
-          data-netlify={true}
-          data-netlify-honeypot="bot-field"
-        >
-          <Field type="hidden" name="contactForm" />
-          <Field type="hidden" name="bot-field" />
-          <label htmlFor="name" className={styles.label}>
-            <p className={styles.formLabel}>
-              Your Name
-            </p>
-            <ErrorMessage name="name" />
-          </label>
-          <Field
-            className={styles.formField}
-            name="name"
-            type="text"
-            placeholder="Name"
-          />
-
-          <label htmlFor="email" className={styles.label}>
-            <p className={styles.formLabel}>
-              Email Address
-            </p>
-            <ErrorMessage name="email" />
-          </label>
-          <Field
-            className={styles.formField}
-            name="email"
-            type="email"
-            placeholder="Email address"
-          />
-
-          <label htmlFor="message" className={styles.label}>
-            <p className={styles.formLabel}>
-              Message
-            </p>
-            <ErrorMessage name="message" />
-          </label>
-          <Field
-            className={styles.formTextField}
-            as="text-area"
-            component="textarea"
-            name="message"
-            placeholder="Type your message to Havamal..."
-          />
-
-          <div>
-            <button className={styles.submitBtn}
-              type="submit"
-            >
-              Submit Message
-            </button>
-          </div>
-        </Form>
-      </div>
-    </Formik>
-  )
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
+  );
 }
