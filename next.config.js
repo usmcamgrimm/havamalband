4f37dc0e40129cc29111f7d242513ac303daa042
Adrian Grimm <usmcamgrimm@gmail.com>
2022-07-20 06:02:11 -0400

    Update the domain for images in the config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
 }
-
-module.exports = nextConfig
