b8ad9218c08036cde566392c6aeee59fe2fb9bdf
Adrian Grimm <usmcamgrimm@gmail.com>
2022-08-15 16:44:48 -0400

    Adding more cloudinary functionality

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/gallery.module.css'
 import Image from 'next/image'
+import { buildUrl } from 'cloudinary-build-url'
 
 export default function Gallery({ photos }) {
   return (
