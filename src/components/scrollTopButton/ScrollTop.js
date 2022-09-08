2e244f1b82574858ef989a1e094eb0308ab2b5b3
Adrian Grimm <usmcamgrimm@gmail.com>
2022-09-08 11:57:49 -0400

    Adding a scroll-to-top button component

---
+++ src/components/scrollTopButton/ScrollTop.js
@@ -0,0 +1,10 @@
+import { BsArrowUp } from 'react-icons/bs'
+import './scrollBtn.css'
+
+export default function scrollTop() {
+  return (
+    <div clasName="top">
+      <BsArrowUp className="scroll-icon" />
+    </div>
+  )
+}
