eb81509c1ba51746db25e9e4e52be108e9ad5b32
Adrian Grimm <usmcamgrimm@gmail.com>
2023-06-22 09:59:39 -0400

    Update lyrics page layout

--- src/pages/lyrics.js
+++ src/pages/lyrics.js
@@ -1,6 +1,11 @@
 import Head from 'next/head'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import styles from '../styles/lyrics.module.css'
 import CallOfTheNorth from '../components/Lyrics/CallOfTheNorth'
+import TalesFromYggdrasil from '../components/Lyrics/TalesFromYggdrasil'
+import TheShadowChapter from '../components/Lyrics/TheShadowChapter'
 
 export default function Lyrics() {
   return (
@@ -14,12 +19,28 @@ export default function Lyrics() {
         Havamal Lyrics
       </h1>
 
-      <section id="Album1">
-        <h2 className={styles.albumTitle}>
-          Call of The North
-        </h2>
-        <CallOfTheNorth />
-      </section>
+      <Container fluid>
+        <Row>
+          <Col sm={12} lg={4}>
+            <h2 className={styles.albumTitle}>
+              Call of The North
+            </h2>
+            <CallOfTheNorth />
+          </Col>
+          <Col sm={12} lg={4}>
+            <h2 className={styles.albumTitle}>
+              Tales From Yggdrasil
+            </h2>
+            <TalesFromYggdrasil />
+          </Col>
+          <Col sm={12} lg={4}>
+            <h2 className={styles.albumTitle}>
+              The Shadow Chapter
+            </h2>
+            <TheShadowChapter />
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
